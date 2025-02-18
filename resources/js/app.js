001881da5202b75499d5ce048b63006e1529032d
waseem abbas <waseemofficee@gmail.com>
2025-02-18 08:16:11 +0500

    set up ui

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,7 +1,12 @@
 import './bootstrap';
 
 import Alpine from 'alpinejs';
+import { createApp } from 'vue';
+
+import App from './App.vue';
 
 window.Alpine = Alpine;
+createApp(App).mount('#app')
 
 Alpine.start();
+
---
+++ resources/js/components/Header.vue
@@ -0,0 +1,9 @@
+<template>
+    <div class=" ">
+        <h1  class="text-[16px] font-bold text-center">Expense Tracker</h1>
+    </div>
+</template>
+
+<script>
+   
+</script>
